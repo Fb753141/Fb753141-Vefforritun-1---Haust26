c82caf5c3bf59b3c9db6a242c63a8c9409fce1cc
Jakob Elias Olafsson <jakob.elias@gmail.com>
2026-09-24 10:24:27 +0000

    vika06

---
+++ aukaVika/javascriptVerkefni/a3/script.js
@@ -0,0 +1,4 @@
+let temperature = 12;
+console.log("Fyrst: "+ temperature +"12°C")
+temperature = temperature + 5;
+console.log("Síðan: " + temperature + "15°C")
---
+++ aukaVika/javascriptVerkefni/a4/script.js
@@ -0,0 +1,5 @@
+const kilometre = 120; 
+let cost = 0;
+console.log("pricePerKm: " + cost +"150kr" ) 
+cost = kilometre * 1.25;
+console.log("Total cost: " + cost + "4200kr")
